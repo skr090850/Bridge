9fe10bc6061926b90c30f56cdaf54fdba8cb1908
skr090850 <skr090850@gmail.com>
2026-06-29 17:55:46 +0530

    API URL Changed

--- lib/Server/server_url.dart
+++ lib/Server/server_url.dart
@@ -1,1 +1,3 @@
-String baseUrl = "http://183.82.115.221/Bridge/BridgeApi/api/";
+// String baseUrl = "http://183.82.115.221/Bridge/BridgeApi/api/";
+// String baseUrl = "http://183.82.115.221/Bridge_New/BridgeApi/";
+String baseUrl = "http://183.82.115.221/Bridge_V3/API/api/";
